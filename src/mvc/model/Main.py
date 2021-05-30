c915024adc55c269e545ab0ed65380115ccdf271
Rodichev Kirill <kirill.rodichev38@gmail.com>
2021-05-30 23:32:16 +0400

    final round

--- src/mvc/model/Main.py
+++ src/mvc/model/Main.py
@@ -1,8 +1,26 @@
 from src.constants.Constants import *
 from src.mvc.controller.EndpointController import Controller
-from src.mvc.controller.MainController import MainController
 from src.mvc.model.InformationTransmissionLine import InformationTransmissionLine
+from src.mvc.model.endpoint.Endpoint import Endpoint
 from src.utils.ExcelExporter import ExcelExporter
+
+
+class MainController:
+
+    def __init__(self):
+        self.endpoints = []
+
+    def generate_endpoints(self):
+        self.endpoints = []
+        for i in range(ENDPOINTS_COUNT):
+            if 0 <= i < 6:
+                self.endpoints.append(Endpoint(CHANNEL_PITCH))
+            elif 6 <= i < 12:
+                self.endpoints.append(Endpoint(CHANNEL_YAW))
+            elif 12 <= i < 18:
+                self.endpoints.append(Endpoint(CHANNEL_ROLL))
+
+        return self.endpoints
 
 
 def main():
